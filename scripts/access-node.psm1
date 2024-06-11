3aa5de13d1138c7744af6ddd8025ecfc48aecdd8
Ivan Baktsheev <dot.and.thing@gmail.com>
2024-06-12 00:52:46 +0300

    feat: remove expired certs; changerd cmdlets names

--- scripts/access-node.psm1
+++ scripts/access-node.psm1
@@ -131,23 +131,16 @@ New-Module -Name BoxForming  -ScriptBlock {
 			break
 		}
 		
-		$Thumbprints = @(Get-ChildItem -Path cert:\LocalMachine\root | Where-Object { $_.Thumbprint -eq $Cert.Thumbprint })
-		
-		if ($Thumbprints.Count -gt 0) {
-			Write-Host "This certificate already imported"
-			break
-		}
-		
-		# import issuing certificate
-		
 		#if ([string]::IsNullOrWhiteSpace($Username)) {
 		#	$Username = $env:UserName
 		#}
 		
+		#region user check
+
 		# remove CN= from cert subject
 		$CertUsername = ($Cert.Subject).Substring(3)
-		
-		# WinRM doesn't allow to add certificate for non-local users:
+
+				# WinRM doesn't allow to add certificate for non-local users:
 		# The WINRM certificate mapping configuration operation cannot be completed because the user credentials
 		# could not be verified.  Please check the username and password used for mapping this certificate and verify that it is
 		# a non-domain account and try again.
@@ -173,6 +166,38 @@ New-Module -Name BoxForming  -ScriptBlock {
 			$Username = $env:UserName
 			Write-Verbose "Certficate's Subject matches current user"
 		}
+
+		#endregion
+
+		#region Remove expired certificates
+
+		$RootStore = "Cert:\LocalMachine\Root"
+		$TrustedPeopleStore = "Cert:\LocalMachine\TrustedPeople"
+
+		[System.Security.Cryptography.X509Certificates.X509Certificate2[]] $ExpiredCerts = @(Get-ChildItem -Path $RootStore | Where-Object { $_.Subject -eq $Cert.Subject -and $_.NotAfter -lt [datetime]::Now })
+
+		if ($ExpiredCerts.Count -gt 0) {
+			Write-Host "Removing expired certificates"
+			# $ExpiredCerts | ForEach-Object { Remove-Cert -StoreName "Root" -Cert $_.Thumbprint }
+
+
+			$ExpiredCerts = @(Get-ChildItem -Path $TrustedPeopleStore | Where-Object { $_.Subject -eq $Cert.Subject -and $_.NotAfter -lt [datetime]::Now })
+			# $ExpiredCerts | ForEach-Object { Remove-Cert -StoreName "Root" -Cert $_.Thumbprint }
+
+			Get-ChildItem -Path WSMan:\localhost\ClientCertificate | Where-Object { $_.Subject -eq "$CertUsername@localhost" } | Remove-Item
+
+		}
+
+		#endregion
+
+		$Thumbprints = @(Get-ChildItem -Path $RootStore | Where-Object { $_.Thumbprint -eq $Cert.Thumbprint })
+		
+		if ($Thumbprints.Count -gt 0) {
+			Write-Host "This certificate already imported"
+			break
+		}
+		
+		# import issuing certificate
 		
 		If (!$Password) {
 			$Password = Read-Host -Prompt "Enter password for $($CertUsername)" -AsSecureString
@@ -231,7 +256,7 @@ New-Module -Name BoxForming  -ScriptBlock {
 		}
 	}
 	
-	Function New-LocalAdminUser {
+	Function New-RemoteAdminUser {
 		param(
 		[Parameter(Mandatory=$true)]
 		[ValidateLength(1,20)]
@@ -248,10 +273,10 @@ New-Module -Name BoxForming  -ScriptBlock {
 		$FullName
 		)
 
-		New-LocalUser(-Username $Username -Password $Password -Description $Description -FullName $FullName -IsAdmin $true)
+		New-RemoteUser(-Username $Username -Password $Password -Description $Description -FullName $FullName -IsAdmin $true)
 	}
 	
-	Function New-LocalUser {
+	Function New-RemoteUser {
 		param(
 		[Parameter(Mandatory=$true)]
 		[ValidateLength(1,20)]
@@ -298,42 +323,25 @@ New-Module -Name BoxForming  -ScriptBlock {
 		
 		# $AdminGroup = Get-CimInstance Win32_Group -Filter "LocalAccount=True AND SID='S-1-5-32-544'"
 		
+		# https://serverfault.com/questions/993482/enable-winrm-for-domain-user
+		# (Get-PSSessionConfiguration -Name Microsoft.PowerShell).Permission
 		if ($IsAdmin) {
 			# [DirectoryServices.AccountManagement.GroupPrincipal]
-			$AdminGroupSid = "S-1-5-32-544"
-			$GroupSidType = [System.DirectoryServices.AccountManagement.IdentityType]::Sid
-			$Group = [System.DirectoryServices.AccountManagement.GroupPrincipal]::FindByIdentity($MachineContext, $GroupSidType, $AdminGroupSid)
-			
-			$Group.Members.Add($PrincipalContext, 'Sid', $User.Sid.Value)
-			
-			$Group.Save()
-			$Group.Dispose()
-			
+			$GroupSid = "S-1-5-32-544"			
+		} else {
+			$GroupSid = "S-1-5-32-580"
 		}
+
+		$GroupSidType = [System.DirectoryServices.AccountManagement.IdentityType]::Sid
+		$Group = [System.DirectoryServices.AccountManagement.GroupPrincipal]::FindByIdentity($MachineContext, $GroupSidType, $GroupSid)
 		
+		$Group.Members.Add($PrincipalContext, 'Sid', $User.Sid.Value)
+		
+		$Group.Save()
+		$Group.Dispose()
+
 		return $User
 		
-	}
-	
-	
-	
-	Function TODO-Enable-WSL {
-		$HaveWSL = $false
-		Try {
-			if (Get-Command wsl.exe) {
-				$HaveWSL = $true
-			}
-		} Catch {
-		}
-		
-		if (!$HaveWSL) {
-			# https://docs.microsoft.com/en-us/windows/wsl/install-on-server
-			Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-			
-			# https://docs.microsoft.com/en-us/windows/wsl/install-manual
-		}
-		
-		# TODO: install ansible
 	}
 	
 	
@@ -345,7 +353,8 @@ New-Module -Name BoxForming  -ScriptBlock {
 		Write-Host "`r`nBoxforming tools for controlled nodes."
 		Write-Host "`r`nWinRM setup for controlled machine:"
 		Write-Host "Enable-WinRM -Verbose"
-		Write-Host "New-LocalAdminUser -Username"
+		Write-Host "New-RemoteUser -Username"
+		Write-Host "New-RemoteAdminUser -Username"
 		Write-Host "Import-ClientAuthCert -Url http://site.com/cert.pem  -File C:\Users\admin\cert.pem"
 		Write-Host "or"
 		Write-Host "Import-ClientAuthCert -File C:\Users\admin\cert.pem"
@@ -353,7 +362,7 @@ New-Module -Name BoxForming  -ScriptBlock {
 		Write-Host ""
 	}
 	
-	Export-ModuleMember -Function 'Initialize-Insomnia','New-LocalAdminUser','Import-ClientAuthCert','Enable-WinRM','New-ClientAuthCert','Start-CertShareServer' -Variable ErrorActionPreference
+	Export-ModuleMember -Function 'Initialize-Insomnia','New-RemoteUser','New-RemoteAdminUser','Import-ClientAuthCert','Enable-WinRM','New-ClientAuthCert','Start-CertShareServer' -Variable ErrorActionPreference
 	
 	#} -AsCustomObject
 	

8525737a30beebacefeeb75269f214f1537b818e
Ivan Baktsheev <dot.and.thing@gmail.com>
2024-06-12 14:58:57 +0300

    fix: messed up controller and node

--- tests/AccessController.Tests.ps1
+++ tests/AccessController.Tests.ps1
@@ -12,18 +12,13 @@ BeforeAll {
 
 Describe "Import-Module BoxFormingAccessController" {
   Context "Certificates" {
-    It "Should be able to generate certificate" {
-      $GeneratedTCert = New-ClientAuthCert -Username "test"
-
-      $GeneratedTCert | Should -Not -BeNullOrEmpty
-    }
 
     It "Should be able to import certificate" {
-      $CertPath = "$env:HOMEDRIVE$env:HOMEPATH\$Username.crt.pem"
+      $CertPath = "$env:HOMEDRIVE$env:HOMEPATH\$Script:Username.crt.pem"
       $CertFromFile = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2
       $CertFromFile.Import($CertPath)
 
-      $CertFromFile.Thumbprint | Should -BeExactly $GeneratedCert.Thumbprint
+      $CertFromFile.Thumbprint | Should -BeExactly $Script:GeneratedCert.Thumbprint
 
       # $Thumbprints = @(Get-ChildItem -Path cert:\LocalMachine\root | Where-Object { $_.Thumbprint -eq $CertFromFile.Thumbprint })
 
@@ -34,29 +29,52 @@ Describe "Import-Module BoxFormingAccessController" {
 
   }
 
-  Context "Creating a new admin user" {
+  Context "PS Web Server" {
 
-    It "Should create new local user" {
+    It "Should be able to start cert share web server" {
+
+      $Root = $PSScriptRoot
+
+      $InitScript = [scriptblock]::Create("Import-Module '$Root\..\scripts\access-controller.psm1' -ArgumentList True -Force")
+
+      $ServerJob = Start-Job -InitializationScript $InitScript -ScriptBlock {
+        Start-CertShareServer -Port 50580 -Username $Input
+      } -InputObject $Script:Username
+
+      Start-Sleep 1.0
+
+      # Write-Host $ServerJob.State
+
+      If ($ServerJob.State -ne "Running") {
+        # $ServerJob | Format-Table | Write-Host
+        Write-Host (Receive-Job $ServerJob) -ForegroundColor Green
+      }
+
+      $ServerJob.State | Should -Be "Running"
+
+      $Uri = "http://localhost:50580/cert.pem"
+
       {
-        New-RemoteAdminUser -Username $Username -Password $Password
+        Invoke-WebRequest -Uri $Uri -UseBasicParsing
       } | Should -Not -Throw
-    }
 
-    It "Should be able to import certificate" {
-      {
-        Import-ClientAuthCert -File "$env:HOMEDRIVE$env:HOMEPATH\$Username.crt.pem" -Password $Password
-      } | Should -Not -Throw
-    }
-  }
+      $CertWebBytes = (Invoke-WebRequest -Uri $Uri -UseBasicParsing).Content
 
-  #Context "Importing certificate" {
+      $CertWebContents = [System.Text.Encoding]::ASCII.GetString($CertWebBytes)
 
+      # $bytes = [System.IO.File]::ReadAllBytes("path_to_the_file")
+      $CertFileContents = [System.IO.File]::ReadAllText("$env:HOMEDRIVE$env:HOMEPATH\$Script:Username.crt.pem")
+
+      $CertWebContents | Should -Be $CertFileContents
 
       #{
       #  Import-ClientAuthCert -Url $Uri -Password $Script:Password
       #} | Should -Throw "The WS-Management service cannot create the resource because it already exists"
 
-  #}
+      Stop-Job $ServerJob
+
+    }
+
+  }
 
 }
-
--- tests/AccessNode.Tests.ps1
+++ tests/AccessNode.Tests.ps1
@@ -4,19 +4,30 @@ Import-Module $PSScriptRoot\..\scripts\access-node.psm1 -Force
 
 Add-Type -AssemblyName System.Web
 
-$Script:Username = "forremote"
-$Script:Password = [System.Web.Security.Membership]::GeneratePassword(16,4) | ConvertTo-SecureString -AsPlainText -Force
+BeforeAll {
+  $Username = "forremote"
+  $Password = [System.Web.Security.Membership]::GeneratePassword(16,4) | ConvertTo-SecureString -AsPlainText -Force
+  $GeneratedCert = New-ClientAuthCert -Username $Username
+}
 
 Describe "Import-Module BoxFormingAccessNode" {
   Context "Certificates" {
 
     It "Should be able to import certificate" {
       {
-        $CertPath = "$env:HOMEDRIVE$env:HOMEPATH\$Script:Username.crt.pem"
+        $CertPath = "$env:HOMEDRIVE$env:HOMEPATH\$Username.crt.pem"
         $CertFromFile = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate2
         $CertFromFile.Import($CertPath)
       } | Should -Not -Throw
 
+      $CertFromFile.Thumbprint | Should -BeExactly $GeneratedCert.Thumbprint
+
+    }
+
+    It "Should be able to generate certificate" {
+      $GeneratedTCert = New-ClientAuthCert -Username "test"
+
+      $GeneratedTCert | Should -Not -BeNullOrEmpty
     }
 
   }
@@ -36,18 +47,22 @@ Describe "Import-Module BoxFormingAccessNode" {
       1 | Should -BeExactly 1
     }
 
-    It "Certificate for non-existing user" {
+    It "Certificate for non-existing user should throw" {
       {
-        Import-ClientAuthCert -File "$env:HOMEDRIVE$env:HOMEPATH\$Script:Username.crt.pem" -Password $Script:Password
+        Import-ClientAuthCert -File "$env:HOMEDRIVE$env:HOMEPATH\$Username.crt.pem" -Password $Password
       } | Should -Throw
     }
 
     It "Should create new local user" {
-      New-RemoteAdminUser -Username $Script:Username -Password $Script:Password
-      Import-ClientAuthCert -File "$env:HOMEDRIVE$env:HOMEPATH\$Script:Username.crt.pem" -Password $Script:Password
+      New-RemoteAdminUser -Username $Username -Password $Password
+      Import-ClientAuthCert -File "$env:HOMEDRIVE$env:HOMEPATH\$Username.crt.pem" -Password $Password
     }
 
-
+    It "Should be able to import certificate" {
+      {
+        Import-ClientAuthCert -File "$env:HOMEDRIVE$env:HOMEPATH\$Username.crt.pem" -Password $Password
+      } | Should -Not -Throw
+    }
 
   }
 
